8661ab08f2574594166f6866b34925ea3604c5a5
LEONARDOMANIVELA <71790507+LEONARDOMANIVELA@users.noreply.github.com>
2020-11-27 18:12:56 -0300

    part11

---
+++ Player/Player.gd
@@ -0,0 +1,63 @@
+extends KinematicBody2D
+
+const ACCELERATION = 500
+const MAX_SPEED = 80
+const FRICTION = 500
+var velocity = Vector2.ZERO
+
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+func _ready():
+	animationTree.active = true
+
+func _physics_process(delta):
+	match state:
+		MOVE: 
+			moveState(delta)
+		
+		ROLL:
+			pass
+	
+		ATTACK:
+			attackState(delta)
+	
+	
+	
+
+func attackAnimationFinished():
+	state = MOVE
+
+func attackState(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+func moveState(delta):
+	var inputVector = Vector2.ZERO
+	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	inputVector = inputVector.normalized()
+	
+	if inputVector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", inputVector)
+		animationTree.set("parameters/Run/blend_position", inputVector)
+		animationTree.set("parameters/Attack/blend_position", inputVector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCELERATION)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
